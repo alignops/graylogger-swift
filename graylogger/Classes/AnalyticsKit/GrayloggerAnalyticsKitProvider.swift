5a0e5d27265acd799b47bd5b79c12d5b4ccb0e5b
PJ Vea <pj.vea@busybusy.com>
2017-10-10 16:22:08 -0600

    Updated logError.

--- graylogger/Classes/AnalyticsKit/GrayloggerAnalyticsKitProvider.swift
+++ graylogger/Classes/AnalyticsKit/GrayloggerAnalyticsKitProvider.swift
@@ -66,52 +66,44 @@ public class GraylogAnalyticsKitProvider: NSObject, AnalyticsKitProvider {
 	}
 
 	// MARK: - Log Errors
-	public func logError(_ name: String, message: String?, exception: NSException?) {
-		if let exception = exception {
-			log(level:.error, message:"Exception - \(name)", longMessage:message, additionalData: [
-				"name": exception.name.rawValue ,
-				"reason": exception.reason ?? "No Reason Given",
-				"userInfo": exception.userInfo ?? "No User Info",
-				"trace": exception.callStackSymbols
-				])
-		}
-		else {
-			log(level:.error, message:"Exception - \(name)", longMessage:message)
-		}
-	}
-
-	public func logError(_ name: String, message: String?, error: Error?) {
-		if let error = error as NSError? {
-			log(level:.error, message:"Error - \(name)", longMessage:message, additionalData: [
-				"domain": error.domain ,
-				"code": error.code ,
-				"description": error.localizedDescription ,
-				"userInfo": error.userInfo,
-				"trace": Thread.callStackSymbols
-				])
-		}
-		else if let error = error {
-			log(level:.error, message:"Error - \(name)", longMessage:message, additionalData: [
-				"description": "\(error)",
-				"trace": Thread.callStackSymbols
-				])
-		}
-		else {
-			log(level:.error, message:"Error - \(name)", longMessage:message)
-		}
-	}
-
-	public func uncaughtException(_ exception: NSException) {
-		logError("Uncaught Exception", message: "Crash on iOS \(UIDevice.current.systemVersion)", exception: exception)
-	}
-    
     public func logError(_ name: String, message: String?, properties: [String : Any]?, exception: NSException?) {
-        log(level:.error, message:"Error - \(name)", longMessage:message)
+        if let exception = exception {
+            log(level:.error, message:"Exception - \(name)", longMessage:message, additionalData: [
+                "name": exception.name.rawValue ,
+                "reason": exception.reason ?? "No Reason Given",
+                "userInfo": exception.userInfo ?? "No User Info",
+                "trace": exception.callStackSymbols
+                ])
+        }
+        else {
+            log(level:.error, message:"Exception - \(name)", longMessage:message)
+        }
     }
     
     public func logError(_ name: String, message: String?, properties: [String : Any]?, error: Error?) {
-        log(level:.error, message:"Error - \(name)", longMessage:message)
+        if let error = error as NSError? {
+            log(level:.error, message:"Error - \(name)", longMessage:message, additionalData: [
+                "domain": error.domain ,
+                "code": error.code ,
+                "description": error.localizedDescription ,
+                "userInfo": error.userInfo,
+                "trace": Thread.callStackSymbols
+                ])
+        }
+        else if let error = error {
+            log(level:.error, message:"Error - \(name)", longMessage:message, additionalData: [
+                "description": "\(error)",
+                "trace": Thread.callStackSymbols
+                ])
+        }
+        else {
+            log(level:.error, message:"Error - \(name)", longMessage:message)
+        }
     }
+    
+	public func uncaughtException(_ exception: NSException) {
+        logError("Uncaught Exception", message: "Crash on iOS \(UIDevice.current.systemVersion)", properties: nil, exception: exception)
+	}
 }
 
 fileprivate extension GraylogAnalyticsKitProvider {
